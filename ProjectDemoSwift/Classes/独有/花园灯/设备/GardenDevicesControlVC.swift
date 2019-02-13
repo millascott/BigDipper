f0feb03398c29fa27b7fdb47432f5075214d7a68
jb <>
2019-02-14 03:25:50 +0800

    no message

---
+++ ProjectDemoSwift/Classes/独有/花园灯/设备/GardenDevicesControlVC.swift
@@ -0,0 +1,30 @@
+//
+//  GardenDevicesControlVC.swift
+//  ProjectDemoSwift
+//
+//  Created by cf on 2019/2/13.
+//  Copyright © 2019年 yons. All rights reserved.
+//
+
+import UIKit
+
+class GardenDevicesControlVC: RootViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
